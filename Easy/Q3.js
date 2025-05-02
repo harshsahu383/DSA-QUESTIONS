3a3fd6cebfbc992652480783dd46e41019c44eee
harshsahu383 <harshsahu1917@gmail.com>
2025-05-03 00:32:30 +0530

    Q3 With Binary Approach

--- Easy/Q3.js
+++ Easy/Q3.js
@@ -5,7 +5,7 @@
 // Q no 35 in leetcode
 // I AM DOING IT WITH LINEAR APPROACH FIRST IT  IS ACCEPTED BY LEET CODE
 let arr = [1,3,5,6];
-let target = 2;
+let target = 7;
 function searchInsertPosition(arr,target){
     let i = 0;
     while(i<arr.length){
@@ -19,3 +19,22 @@ function searchInsertPosition(arr,target){
     return arr.length;
 }
 console.log(searchInsertPosition(arr,target));
+// With Binary Search Algorithm
+function binarySearch(arr,target){
+    let left = 0;
+    let right = arr.length-1;
+    while(left<=right){
+        let = mid = Math.floor((left+right)/2)
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[mid]<target){
+            left = mid+1;
+        }
+        else{
+            right = mid -1;
+        }
+    }
+    return left;
+}
+console.log(binarySearch(arr,target));
