43f271b9205e8e40b263b74a7f89eb8178359829
harshsahu383 <harshsahu1917@gmail.com>
2025-05-04 16:36:41 +0530

    java variables

---
+++ JAVA/Variables.java
@@ -0,0 +1,10 @@
+  class Variables{
+   static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+     int result = add(5,10);
+     System.out.println("Sum :" + result);
+
+    }
+}
