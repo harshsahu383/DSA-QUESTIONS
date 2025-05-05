a32634ff5d9a415789960cd53584526f897d45a4
harshsahu383 <harshsahu1917@gmail.com>
2025-05-06 00:26:00 +0530

    Q15.js mediuem modify

--- Medium/Array/Q15.js
+++ Medium/Array/Q15.js
@@ -2,26 +2,27 @@
 // Qno 15 mediuem in leetcode array
 //Notice that the solution set must not contain duplicate triplets.
 // let arr = [-1,-5,0,-3]
-// function threeSum(arr){
-//     let i = 0,left = i + 1,right = arr.length-1;
-//     let result = [];
-//     arr.sort();
-//     while(left<=right){
-//         if(arr[i] + arr[left] + arr[right]==0){
-//          result = result.shift(arr[i],arr[left],arr[right])
-//             left++;
-//             right--;
-//         }
-//         else if(arr[i] + arr[left]+ arr[right] < 0){
-//             left++;  
-//         }
-//         else{
-//             right--;
-//         }
-//     }
-//     return result;
-// }
-// threeSum(arr);4
+function threeSum(arr){
+    let i = 0,left = i + 1,right = arr.length-1;
+    let result = [];
+    arr.sort();
+    while(left<=right){
+        if(arr[i] + arr[left] + arr[right]==0){
+         result = result.shift(arr[i],arr[left],arr[right])
+            left++;
+            right--;
+        }
+        else if(arr[i] + arr[left]+ arr[right] < 0){
+            left++;  
+        }
+        else{
+            right--;
+        }
+    }
+    return result;
+}
+threeSum(arr);
+// two pointer approach
 // sorting with two pointer is optimal approach for solving this
 // this is not the best way but its efficient way to solve this types of problems
 /**
@@ -33,13 +34,13 @@ var threeSum = function(nums) {
    nums.sort((a,b) => a -b);
    const result = [];
    for(let i = 0; i< nums.length-2;i++){
-    if(i<0 && nums[i] === nums[i-1]) continue;
+    if(i>0 && nums[i] === nums[i-1]) continue;
     let left = i + 1;
     let right = nums.length-1;
     while(left<right){
-        const sum  = nums[i] + nums[left] + nums;[right];
+        const sum  = nums[i] + nums[left] + nums[right];
         if(sum === 0){
-            result.push(nusm[i],nums[left],nums[right])
+            result.push([nums[i],nums[left],nums[right]])
             while(left<right && nums[left] === nums[left + 1]) left++;
             while(left<right && nums[right] === nums[right - 1]) right--;
             left++;
