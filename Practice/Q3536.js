32cc17d2067bfac83a407ad45aca1feabf79c310
harshsahu383 <harshsahu1917@gmail.com>
2025-05-06 01:17:04 +0530

    Q3536 Weekly contest

--- Practice/Q3536.js
+++ Practice/Q3536.js
@@ -9,7 +9,7 @@
 // The digits of n are [3, 1].
 // The possible products of any two digits are: 3 * 1 = 3.
 // The maximum product is 3.
-let n = 12
+let n = 437
 function num(n){
   let arr = [],
     max = 0;
@@ -17,10 +17,25 @@ function num(n){
     arr.unshift(n % 10);
     n = Math.floor(n / 10);
   }
-  for (let i = 0; i < arr.length - 1; i++) {
-    let currentMax = arr[i] * arr[i + 1];
-    max = Math.max(currentMax, max);
-  }
+  arr.sort((a,b)=> a-b);
+  max = arr[arr.length-1] * arr[arr.length-2];
   return max;
 }
 console.log(num(n));
+// this solution is run on 1ms time we have 0 ms time solutuon 
+// 0ms runtime //
+function digit(n){
+     let max1 = 0;
+    let max2 = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        if (digit > max1) {
+            max2 = max1;
+            max1 = digit;
+        } else if (digit > max2) {
+            max2 = digit;
+        }
+        n = Math.floor(n / 10);
+    }
+    return max1 * max2;
+}
