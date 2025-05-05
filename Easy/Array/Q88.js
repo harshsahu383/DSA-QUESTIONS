a6b7fc833ca6eedf5279a46daeb960f5f141d4c1
harshsahu383 <harshsahu1917@gmail.com>
2025-05-05 23:56:24 +0530

    Q88.js easy

---
+++ Easy/Array/Q88.js
@@ -0,0 +1,26 @@
+// Q88 in leetcode merge arrays 
+// We can do it with the three pointer approach as well as modern solution for it i write both but first three pointer approach
+// three pointer approach 
+let nums1 = [1,2,3,0,0,0];
+let m  = 3;// real values present in nums1
+let nums2 = [2,4,5];
+let n = nums2.length;
+function mergeArrays(nums1,m,nums2,n){
+      let i = m - 1; // Last index of real nums1 elements
+      let j = n - 1; // Last index of nums2
+      let k = m + n - 1; // Last index of nums1 (full size)
+
+      while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+          nums1[k] = nums1[i];
+          i--;
+        } else {
+          nums1[k] = nums2[j];
+          j--;
+        }
+        k--;
+      }
+    return nums1
+
+};
+console.log(mergeArrays(nums1,m,nums2,n));
