28be5472da592c7cee2d6dd26b8898ad594cb396
harshsahu383 <harshsahu1917@gmail.com>
2025-05-06 23:55:47 +0530

    Q118 pascals traingle

---
+++ Easy/Array/Q118.js
@@ -0,0 +1,3 @@
+//Q118 the pascals traingle in the leet code 
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
