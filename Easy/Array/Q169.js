6843d9dd7d980a60cd468a27e757390dd0ddc2b5
harshsahu383 <harshsahu1917@gmail.com>
2025-05-08 22:57:31 +0530

    Q169 Easy leet code

--- Easy/Array/Q169.js
+++ Easy/Array/Q169.js
@@ -1,15 +1,16 @@
-// // Given an array nums of size n, return the majority element.
+// Given an array nums of size n, return the majority element.
 
-// // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-// function majorityElement(nums) {
-//     let count = 0;
-//     let candidate = null;
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+function majorityElement(nums) {
+    let count = 0;
+    let candidate = null;
 
-//     for (let num of nums) {
-//         if (count === 0) {
-//             candidate = num;
-//         }
-//         count += (num === candidate) ? 1 : -1;
-//     }
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
 
-//     return candidate;
+    return candidate;
+}
