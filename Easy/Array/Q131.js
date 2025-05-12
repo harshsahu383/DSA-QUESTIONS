43d4847ce0ad439dbd60350f4e18c31d3cdac244
harshsahu383 <harshsahu1917@gmail.com>
2025-05-12 23:59:33 +0530

    131 Easy

---
+++ Easy/Array/Q131.js
@@ -0,0 +1,23 @@
+
+
+
+function buyAndSellStock(prices){
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+      if (prices[i] < minPrice) {
+        minPrice = prices[i];
+      } else {
+        let profit = prices[i] - minPrice;
+        if (profit > maxProfit) {
+          maxProfit = profit;
+        }
+      }
+    }
+
+    return maxProfit;
+
+}
+let prices = [7,1,2,3,4,5,6];
+console.log(buyAndSellStock(prices));
