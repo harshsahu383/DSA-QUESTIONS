96f5042a1301db7e1edd4fff232c086609498153
harshsahu383 <harshsahu1917@gmail.com>
2025-05-13 23:43:21 +0530

    Q217

---
+++ Easy/Array/Q217.js
@@ -0,0 +1,11 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+function duplicate(nums){
+    let seen = new Set();
+    for(num of nums){
+        if(seen.has(num)){
+            return true;
+        }
+        seen.add(num);
+    }
+    return false;
+}
