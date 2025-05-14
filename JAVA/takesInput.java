69a417e18ffe543c8a6ff0b2d63c2a0d330b9439
harshsahu383 <harshsahu1917@gmail.com>
2025-05-14 23:57:01 +0530

    java

---
+++ JAVA/takesInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class takesInput {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int num_1 = sc.nextInt();
+
+        System.out.println("Your number is :" + num_1);
+    }
+}
