f566cf24deeb0580aad23e930589b4d01b4d59d9
harshsahu383 <harshsahu1917@gmail.com>
2025-05-15 21:32:58 +0530

    java

---
+++ JAVA/ReadChar.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class ReadChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string : ");
+        char s = sc.next().charAt(0);
+        
+        System.out.println("char is : " + s);
+        
+    }
+}
