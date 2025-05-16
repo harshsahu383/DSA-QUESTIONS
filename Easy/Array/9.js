03b4c57fce2dd0046f019886b60e2125c210a562
harshsahu383 <harshsahu1917@gmail.com>
2025-05-16 23:15:23 +0530

    Q9

--- Easy/Array/9.js
+++ Easy/Array/9.js
@@ -1,5 +1,6 @@
 // Q9 in leet code for palindrome 
 // Given an integer x, return true if x is a palindrome, and false otherwise.
+//this is not the very fast way to solve this problem
 function findPalindrome (x){
  if (x<0) return false;4
  let str = x.toString();
