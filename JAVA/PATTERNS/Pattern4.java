19184b69fca4f0ad9be6302e3b233a2853fd2532
harshsahu383 <harshsahu1917@gmail.com>
2025-05-17 23:47:28 +0530

    Q75

--- JAVA/PATTERNS/Pattern4.java
+++ JAVA/PATTERNS/Pattern4.java
@@ -2,7 +2,6 @@ package PATTERNS;
 public class Pattern4 {
     public static void main(String[] args) {
         int r = 4;
-        int c = 1;
         for(int i = 1;i<=r;i++){
             for(int j = 1;j<=r-i;j++){
                 System.out.print(" ");
