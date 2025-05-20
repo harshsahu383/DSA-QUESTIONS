842c30929a64251fb22288917096f7622543a9a3
harshsahu383 <harshsahu1917@gmail.com>
2025-05-20 18:30:03 +0530

    Q3355

---
+++ Practice/Q3355.js
@@ -0,0 +1,28 @@
+// You are given an integer array nums of length n and a 2D array queries, where queries[i] = [li, ri].
+
+// For each queries[i]:
+
+// Select a subset of indices within the range [li, ri] in nums.
+// Decrement the values at the selected indices by 1.
+// A Zero Array is an array where all elements are equal to 0.
+
+// Return true if it is possible to transform nums into a Zero Array after processing all the queries sequentially, otherwise return false.
+function findZero(nums){
+    const deltaArray = new Array(nums.length + 1).fill(0);
+    for (const [left, right] of queries) {
+      deltaArray[left] += 1;
+      deltaArray[right + 1] -= 1;
+    }
+    const operationCounts = [];
+    let currentOperations = 0;
+    for (const delta of deltaArray) {
+      currentOperations += delta;
+      operationCounts.push(currentOperations);
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (operationCounts[i] < nums[i]) {
+        return false;
+      }
+    }
+    return true;
+}
