7bfa75767962c63925b9f9bed2989fc8da108b57
harshsahu383 <harshsahu1917@gmail.com>
2025-05-21 23:24:15 +0530

    Q448

--- Easy/Array/448.js
+++ Easy/Array/448.js
@@ -15,3 +15,22 @@ var findDisappearedNumbers = function (nums) {
 
   return result
 };
+   function findMissing(nums){
+
+       for (let i = 0; i < nums.length; i++) {
+           const index = Math.abs(nums[i]) - 1;
+           if (nums[index] > 0) {
+               nums[index] = -nums[index];
+            }
+    }
+
+  
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            result.push(i + 1);
+        }
+    }
+    
+    return result;
+}
