0fdeafc11edf4d6c082baf5d416dc0ace064f1fd
harshsahu383 <harshsahu1917@gmail.com>
2025-05-21 23:23:23 +0530

    Q448

--- Easy/Array/448.js
+++ Easy/Array/448.js
@@ -1,5 +1,6 @@
 // // Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums
 // Find All Numbers Disappeared In Array
+// this is also a good but this is not the great aaproach to do this
 var findDisappearedNumbers = function (nums) {
   let result = [];
   let n = 1;
