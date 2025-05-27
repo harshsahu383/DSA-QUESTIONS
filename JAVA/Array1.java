da1105c0c73936120682e4669c822ab9d52b713f
harshsahu383 <harshsahu1917@gmail.com>
2025-05-28 01:17:02 +0530

    arrays

--- JAVA/Array1.java
+++ JAVA/Array1.java
@@ -1,14 +1,15 @@
 import java.util.*;
 public class Array1{
-     static int find(int[] arr,int x){
-        int count = 0;
+     static int[] find(int[] arr,int x){
+        int count = 0,last = 0;
         for(int i = 0;i<arr.length;i++){
             if(arr[i] == x){
                 count++;
+                last = i;
             }
         }
-      return count ;
-     }
+        return new int[] { count, last};  
+       }
     public static void main(String[] args) {
         int [] arr = {0,1,2,1,1,3};
         int x = 1;
