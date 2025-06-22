d58e97bc5e9f36d0d08144a99c65556e8f142f58
harshsahu383 <harshsahu1917@gmail.com>
2025-06-22 23:18:47 +0530

    Q2138

---
+++ Practice/Q2138.js
@@ -0,0 +1,38 @@
+// A string s can be partitioned into groups of size k using the following procedure:
+// Q2138  Divide a String Into Groups of Size k
+// The first group consists of the first k characters of the string, the second group consists of the next k characters of the string, and so on. Each element can be a part of exactly one group.
+// For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+// Note that the partition is done so that after removing the fill character from the last group (if it exists) and concatenating all the groups in order, the resultant string should be s.
+
+// Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcdefghi", k = 3, fill = "x"
+// Output: ["abc","def","ghi"]
+// Explanation:
+// The first 3 characters "abc" form the first group.
+// The next 3 characters "def" form the second group.
+// The last 3 characters "ghi" form the third group.
+// Since all groups can be completely filled by characters from the string, we do not need to use fill.
+// Thus, the groups formed are "abc", "def", and "ghi"
+
+var divideString = function (s, k, fill) {
+  const res = []; // grouped string
+  const n = s.length;
+  let curr = 0; // starting index of each group
+  // split string
+  while (curr < n) {
+    const end = Math.min(curr + k, n);
+    res.push(s.slice(curr, end));
+    curr += k;
+  }
+  // try to fill in the last group
+  const last = res[res.length - 1];
+  if (last.length < k) {
+    res[res.length - 1] = last + fill.repeat(k - last.length);
+  }
+  return res;
+};
